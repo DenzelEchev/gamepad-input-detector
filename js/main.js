8edeff4ca49eb1eea9dc31228deeff83fe13c9fb
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-05-12 05:44:19 -0400

    added better code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
-const gamepad = navigator.getGamepads()[0];
-
 // Add an event listener to check for button presses/releases
-window.addEventListener("gamepadbuttondown", (event) => {
-  const buttonIndex = event.detail.index; // get the index of the pressed button
-  const buttonValue = gamepad.buttons[buttonIndex].value; // get the current value of the button
-  console.log(`Button ${buttonIndex} pressed with value ${buttonValue}`);
-});
+window.addEventListener("gamepadconnected", (event) => {
+    console.log("Gamepad connected!");
+    const gamepad = event.gamepad;
+  
+    // Add event listeners for button presses/releases
+    window.addEventListener("gamepadbuttondown", (event) => {
+      const buttonIndex = event.detail.index; // get the index of the pressed button
+      const buttonValue = gamepad.buttons[buttonIndex].value; // get the current value of the button
+      console.log(`Button ${buttonIndex} pressed with value ${buttonValue}`);
+    });
+  
+    window.addEventListener("gamepadbuttonup", (event) => {
+      const buttonIndex = event.detail.index; // get the index of the released button
+      const buttonValue = gamepad.buttons[buttonIndex].value; // get the current value of the button
+      console.log(`Button ${buttonIndex} released with value ${buttonValue}`);
+    });
+  });
+  
+  // Add an event listener to check for disconnection of gamepad
+  window.addEventListener("gamepaddisconnected", (event) => {
+    console.log("Gamepad disconnected");
+  });
+  
